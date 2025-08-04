59235974020f815584ca6cee8f6424db49f983d3
Erick Zanardo <erickzanardoo@gmail.com>
2025-08-04 11:20:03 -0300

    feat: Adding hitboxes support to atlas

--- typled/test/src/typled_atlas_test.dart
+++ typled/test/src/typled_atlas_test.dart
@@ -23,5 +23,36 @@ sprite3 = 0,0,1,2
       expect(atlas.sprites['sprite2'], (0, 0, 1, null));
       expect(atlas.sprites['sprite3'], (0, 0, 1, 2));
     });
+
+    group('when hitboxes are specified', () {
+      test('parses hitboxes correctly', () {
+        const rawContent = '''
+[atlas]
+imagePath = assets/atlas.png
+tileSize = 16
+
+[sprites]
+player = 0,0
+sprite2 = 0,0,1
+sprite3 = 0,0,1,2
+
+[hitboxes]
+player = 0,0
+sprite2 = 0,0,1,1
+sprite3 = 0,0,2,1
+''';
+
+        final atlas = TypledAtlas.parse(rawContent);
+        expect(atlas.imagePath, 'assets/atlas.png');
+        expect(atlas.tileSize, 16);
+        expect(atlas.sprites['player'], (0, 0, null, null));
+        expect(atlas.sprites['sprite2'], (0, 0, 1, null));
+        expect(atlas.sprites['sprite3'], (0, 0, 1, 2));
+
+        expect(atlas.hitboxes!['player'], (0, 0, null, null));
+        expect(atlas.hitboxes!['sprite2'], (0, 0, 1, 1));
+        expect(atlas.hitboxes!['sprite3'], (0, 0, 2, 1));
+      });
+    });
   });
 }
